03727fadb08a5459f9f459bc4f66c1b7da6478a0
Hector Martinez <87312991+rh-hemartin@users.noreply.github.com>
2024-05-17 13:11:56 +0200

    Use Server struct to share things (#28)
    * Add struct with k8sclient and router.
    * Created a test passing a fake k8sclient.
    
    Close #27
    
    Signed-off-by: Hector Martinez <hemartin@redhat.com>

--- cmd/api/main_test.go
+++ cmd/api/main_test.go
@@ -1,43 +1,63 @@
 // Copyright KubeArchive Authors
 // SPDX-License-Identifier: Apache-2.0
-
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"net/http/httputil"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
-func TestRouterInvalidMethods(t *testing.T) {
-	router := setupRouter()
+func TestNewServer(t *testing.T) {
+	server := NewServer(fake.NewSimpleClientset())
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	server.router.ServeHTTP(res, req)
 
+	assert.Equal(t, http.StatusUnauthorized, res.Code)
+}
+
+func TestAuthenticationMiddlewareIsConfigured(t *testing.T) {
 	tests := []struct {
-		name   string
-		method string
+		name     string
+		method   string
+		expected int
 	}{
 		{
-			name:   "Invalid POST",
-			method: "POST",
+			name:     "Invalid POST",
+			method:   "POST",
+			expected: http.StatusUnauthorized,
 		},
 		{
-			name:   "Invalid PUT",
-			method: "PUT",
+			name:     "Invalid PUT",
+			method:   "PUT",
+			expected: http.StatusUnauthorized,
 		},
 		{
-			name:   "Invalid DELETE",
-			method: "DELETE",
+			name:     "Invalid DELETE",
+			method:   "DELETE",
+			expected: http.StatusUnauthorized,
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
+			server := NewServer(fake.NewSimpleClientset())
 			res := httptest.NewRecorder()
 			req, _ := http.NewRequest(tc.method, "/apis/stable.example.com/v1/crontabs", nil)
-			router.ServeHTTP(res, req)
+			server.router.ServeHTTP(res, req)
 
-			assert.Equal(t, http.StatusNotFound, res.Code)
+			b, err := httputil.DumpResponse(res.Result(), true)
+			if err != nil {
+				t.Error(err)
+				t.FailNow()
+			}
+
+			assert.Equal(t, tc.expected, res.Code, string(b))
 		})
 	}
 }

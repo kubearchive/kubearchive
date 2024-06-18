bf1d68b2d19f67fc332dd309eccc57ad7be421b5
skoved <skoved@redhat.com>
2024-06-18 16:05:26 -0400

    sink writes cloudevents to the database
    Need to make this work with the full db schema, clean up the code, and
    figure out a better solution for connecting to the database. This works
    though. You just need to deploy the database before installing
    kubearchive and update the host to use the IP of the database service
    
    Signed-off-by: skoved <skoved@redhat.com>

--- cmd/sink/main.go
+++ cmd/sink/main.go
@@ -5,22 +5,158 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	ceOtelObs "github.com/cloudevents/sdk-go/observability/opentelemetry/v2/client"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	ceClient "github.com/cloudevents/sdk-go/v2/client"
+	"github.com/cloudevents/sdk-go/v2/types"
 	kaObservability "github.com/kubearchive/kubearchive/pkg/observability"
+	_ "github.com/lib/pq"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
-var logger = log.New(os.Stderr, "", log.LstdFlags|log.Lmicroseconds|log.LUTC)
+const (
+	connStr             = "user=kubearchive password=kubearchive dbname=kubearchive-db host=postgres.database.svc.cluster.local port=5432 sslmode=disable"
+	apiVersionExtension = "apiversion"
+	kindExtension       = "kind"
+	nameExtension       = "name"
+	namespaceExtension  = "namespace"
+)
 
+var (
+	logger         = log.New(os.Stderr, "", log.LstdFlags|log.Lmicroseconds|log.LUTC)
+	db     *sql.DB = nil
+)
+
+// Represents the fields in a resource's metadata that are need to write a resource into the database
+type ResourceMetadata struct {
+	ResourceVersion string `json:"resourceVersion"`
+}
+
+// Respresents the fields in the data field of a cloudevent to write a resource into the database
+type ResourceData struct {
+	Created     string           `json:"firstTimestamp"`
+	LastUpdated string           `json:"lastTimestamp"`
+	Metadata    ResourceMetadata `json:"metadata"`
+}
+
+// checks that the cloudevents has the appropriate extensions set and have values that are the right type. Additionally
+// it checks that the cloudevent's data has the necessary fields. If all conditions are not met, it returns an error
+func validateCloudevent(event cloudevents.Event) error {
+	eventExtensions := event.Extensions()
+	_, err := types.ToString(eventExtensions[apiVersionExtension])
+	if err != nil {
+		return err
+	}
+	_, err = types.ToString(eventExtensions[kindExtension])
+	if err != nil {
+		return err
+	}
+	_, err = types.ToString(eventExtensions[nameExtension])
+	if err != nil {
+		return err
+	}
+	_, err = types.ToString(eventExtensions[namespaceExtension])
+	if err != nil {
+		return err
+	}
+	payload := ResourceMetadata{}
+	err = event.DataAs(&payload)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Writes the kubernetes resource to the database. This method must be called after calling validateCloudevent and it
+// does not return an error
+func writeToDatabase(event cloudevents.Event) {
+	var err error
+	if db == nil {
+		logger.Println("opening connection to the database")
+		db, err = sql.Open("postgres", connStr)
+		if err != nil {
+			logger.Printf("failed to connect to database: %s", err)
+			return
+		}
+	}
+
+	eventExtensions := event.Extensions()
+	// since a call to validateCloudevent should already have passed we can assume that all of these values exist as
+	// strings
+	apiVersion := eventExtensions[apiVersionExtension]
+	kind := eventExtensions[kindExtension]
+	name := eventExtensions[nameExtension]
+	namespace := eventExtensions[namespaceExtension]
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		logger.Printf("could not begin transaction: %s", err)
+		cancel()
+		return
+	}
+	resourceData := ResourceData{}
+	err = event.DataAs(&resourceData)
+	if err != nil {
+		logger.Printf("Canceling database transaction. Could not parse data from the cloudevent: %s\n", err)
+		rollbackErr := tx.Rollback()
+		if rollbackErr != nil {
+			logger.Printf("Could not rollback transaction: %s\n", rollbackErr)
+		}
+		cancel()
+		return
+	}
+	resource := string(event.Data())
+	_, execErr := tx.ExecContext(ctx,
+		"INSERT INTO ArchiveMeta (api_version, kind, name, namespace, resource_version, data, created_ts, updated_ts) Values ($1, $2, $3, $4, $5, $6, $7, $8)",
+		apiVersion,
+		kind,
+		name,
+		namespace,
+		resourceData.Metadata.ResourceVersion,
+		resource,
+		resourceData.Created,
+		resourceData.LastUpdated,
+	)
+	if execErr != nil {
+		logger.Printf("Rolling back transaction. Write to database failed: %s\n", execErr)
+		rollbackErr := tx.Rollback()
+		if rollbackErr != nil {
+			logger.Printf("unable to rollback transaction: %s", rollbackErr)
+		}
+		cancel()
+		return
+	}
+	execErr = tx.Commit()
+	if execErr != nil {
+		logger.Printf("Database transaction failed: %s\n", execErr)
+		rollbackErr := tx.Rollback()
+		if rollbackErr != nil {
+			logger.Printf("unable to rollback transaction: %s\n", rollbackErr)
+		}
+		cancel()
+		return
+	}
+	cancel()
+	logger.Println("transaction completed successfully")
+}
+
+// Processes incoming cloudevents and
 func receive(event cloudevents.Event) {
 	logger.Println("received CloudEvent: ", event.ID())
-	logger.Printf("%s\n", event.String())
+	err := validateCloudevent(event)
+	if err != nil {
+		logger.Printf("cloudevent is malformed and will not be processed: %s\n", err)
+		return
+	}
+	writeToDatabase(event)
 }
 
 func main() {

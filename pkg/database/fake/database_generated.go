a1b86e9e3ad8ccef69d905eeaa6bb41ab32d2b3e
skoved <skoved@redhat.com>
2024-07-11 14:49:28 -0400

    remove unneeded fake database methods
    Signed-off-by: skoved <skoved@redhat.com>

--- pkg/database/fake/database_generated.go
+++ pkg/database/fake/database_generated.go
@@ -26,23 +26,6 @@ func (f *Database) QueryResources(ctx context.Context, kind, group, version stri
 	return filteredResources, nil
 }
 
-func (f *Database) QueryResourceId(ctx context.Context, entry *models.ResourceEntry) (int64, error) {
-	for id, dbEntry := range f.entries {
-		if dbEntry.Kind == entry.Kind && dbEntry.ApiVersion == entry.ApiVersion && dbEntry.Cluster == entry.Cluster && dbEntry.ClusterUid == entry.ClusterUid && dbEntry.Name == entry.Name && dbEntry.Namespace == entry.Namespace {
-			return int64(id), nil
-		}
-	}
-	return 0, fmt.Errorf("no entry found")
-}
-
-func (f *Database) UpdateResource(ctx context.Context, id int64, entry *models.ResourceEntry) error {
-	if len(f.entries) <= int(id) {
-		return fmt.Errorf("id %d not in database", id)
-	}
-	f.entries[id] = *entry
-	return nil
-}
-
 func (f *Database) WriteResource(ctx context.Context, entry *models.ResourceEntry) error {
 	f.entries = append(f.entries, *entry)
 	return nil

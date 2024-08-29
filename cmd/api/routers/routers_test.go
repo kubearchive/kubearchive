74181053e7d7dc5a6698e664d7e2ea65445a7055
Hector Martinez <87312991+rh-hemartin@users.noreply.github.com>
2024-08-29 16:17:23 +0200

    API: Return List resource instead of JSON list (#359)
    Signed-off-by: Hector Martinez <hemartin@redhat.com>

--- cmd/api/routers/routers_test.go
+++ cmd/api/routers/routers_test.go
@@ -14,6 +14,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
+	"github.com/kubearchive/kubearchive/pkg/database"
 	"github.com/kubearchive/kubearchive/pkg/database/fake"
 )
 
@@ -26,9 +27,9 @@ var testAPIResource = metav1.APIResource{
 	SingularName: "crontab",
 	Namespaced:   true}
 
-func setupRouter() *gin.Engine {
+func setupRouter(db database.DBInterface) *gin.Engine {
 	router := gin.Default()
-	ctrl := Controller{Database: fake.NewFakeDatabase(testResources)}
+	ctrl := Controller{Database: db}
 	router.Use(func(c *gin.Context) {
 		c.Set("apiResource", testAPIResource)
 		c.Next()
@@ -39,31 +40,46 @@ func setupRouter() *gin.Engine {
 }
 
 func TestGetAllResources(t *testing.T) {
-	router := setupRouter()
+	router := setupRouter(fake.NewFakeDatabase(testResources))
 
 	res := httptest.NewRecorder()
 	req, _ := http.NewRequest(http.MethodGet, "/apis/stable.example.com/v1/crontabs", nil)
 	router.ServeHTTP(res, req)
 
 	assert.Equal(t, http.StatusOK, res.Code)
-	var resources []*unstructured.Unstructured
+	var resources List
 	if err := json.NewDecoder(res.Body).Decode(&resources); err != nil {
 		t.Fail()
 	}
-	assert.Equal(t, resources, testResources)
+	assert.Equal(t, NewList(testResources), resources)
 }
 
 func TestGetNamespacedResources(t *testing.T) {
-	router := setupRouter()
+	router := setupRouter(fake.NewFakeDatabase(testResources))
 
 	res := httptest.NewRecorder()
 	req, _ := http.NewRequest(http.MethodGet, "/apis/stable.example.com/v1/namespace/ns/crontabs", nil)
 	router.ServeHTTP(res, req)
 
 	assert.Equal(t, http.StatusOK, res.Code)
-	var resources []*unstructured.Unstructured
+	var resources List
 	if err := json.NewDecoder(res.Body).Decode(&resources); err != nil {
 		t.Fail()
 	}
-	assert.Equal(t, resources, testResources)
+	assert.Equal(t, NewList(testResources), resources)
+}
+
+func TestGetResourcesEmpty(t *testing.T) {
+	router := setupRouter(fake.NewFakeDatabase([]*unstructured.Unstructured{}))
+
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/apis/stable.example.com/v1/namespace/ns/crontabs", nil)
+	router.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusOK, res.Code)
+	var resources List
+	if err := json.NewDecoder(res.Body).Decode(&resources); err != nil {
+		t.Fail()
+	}
+	assert.Equal(t, NewList(nil), resources)
 }

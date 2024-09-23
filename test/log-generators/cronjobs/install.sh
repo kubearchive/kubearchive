12f46a8d0813fcac7ed2c360e060b11a6f288301
ggallen <ggallen@users.noreply.github.com>
2024-09-23 16:30:38 -0400

    Update CronJob log generator to take a namespace parameters. (#446)
    Signed-off-by: Greg Allen <gallen@redhat.com>

--- test/log-generators/cronjobs/install.sh
+++ test/log-generators/cronjobs/install.sh
@@ -1,9 +1,48 @@
 #!/bin/bash
+# Copyright KubeArchive Authors
+# SPDX-License-Identifier: Apache-2.0
 
 # Create a CronJob that run every minute, creating an Apache log file 1024 lines long.
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-namespace="generate-logs-cronjobs"
-kubectl create ns ${namespace}
-kubectl -n ${namespace} apply -f ${SCRIPT_DIR}
+# Parse command line arguments
+for i in "$@"
+do
+case $i in
+    --namespace=*)
+    NAMESPACE=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    --help)
+    HELP=True
+    ;;
+    *)
+    echo "Unknown option $i" # unknown option
+    HELP=True
+    UNKNOWN=True
+    ;;
+esac
+done
+
+HELP=${HELP:-""}
+UNKNOWN=${UNKNOWN:-""}
+export NAMESPACE=${NAMESPACE:-generate-logs-cronjobs}
+
+# Help and usage
+if [ "${HELP}" == "True" ] || [ "${UNKNOWN}" == "True" ]; then
+    echo -e "$0
+
+    --namespace    Namespace to use to generate logs.
+                   Default value is ${NAMESPACE}
+
+    "
+    if [ "${UNKNOWN}" == "True" ]; then
+      exit 1;
+    else
+      exit 0;
+    fi
+fi
+
+
+kubectl create ns ${NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
+kubectl -n ${NAMESPACE} apply -f ${SCRIPT_DIR}

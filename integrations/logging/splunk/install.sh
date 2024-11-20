bf023282facc9da38bf7a15e0732dedbf5397b32
ggallen <ggallen@users.noreply.github.com>
2024-11-20 09:01:22 -0500

    Document and initiate new logging strategy to return log file content. (#655)
    Signed-off-by: Greg Allen <gallen@redhat.com>

--- integrations/logging/splunk/install.sh
+++ integrations/logging/splunk/install.sh
@@ -8,13 +8,22 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd ${SCRIPT_DIR}
 
+NAMESPACE="splunk-operator"
+
 kubectl apply -f https://github.com/splunk/splunk-operator/releases/download/2.6.0/splunk-operator-cluster.yaml \
 	--server-side --force-conflicts
 
 helm upgrade --install --wait --create-namespace \
-	--namespace splunk-operator \
+	--namespace ${NAMESPACE} \
 	logging-operator oci://ghcr.io/kube-logging/helm-charts/logging-operator
 
-kubectl -n splunk-operator apply -f .
+kubectl -n ${NAMESPACE} apply -f .
 
-kubectl rollout status deployment --namespace=splunk-operator --timeout=90s
+kubectl rollout status deployment --namespace=${NAMESPACE} --timeout=90s
+
+# If KubeArchive is installed, updated the password.
+KUBEARCHIVE_NS="kubearchive"
+if kubectl get ns ${KUBEARCHIVE_NS} >& /dev/null; then
+    SPLUNK_PWD=$(kubectl -n ${NAMESPACE} get secret splunk-splunk-operator-secret -o jsonpath='{.data.password}')
+    kubectl patch -n ${KUBEARCHIVE_NS} secret kubearchive-logging -p "{\"data\": {\"password\": \"${SPLUNK_PWD}\"}}"
+fi

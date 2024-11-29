f6040c199fb145d19e88ddcef3dab53b0f0c4e9f
M. Andrea Fernández <manferna@redhat.com>
2024-11-29 09:08:00 +0100

    Set default timezone in PostgreSQL
    Signed-off-by: M. Andrea Fernández <manferna@redhat.com>

--- integrations/database/postgresql/kubearchive.sql
+++ integrations/database/postgresql/kubearchive.sql
@@ -40,6 +40,26 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: kubearchive; Type: DATABASE PROPERTIES; Schema: -; Owner: kubearchive
+--
+
+ALTER DATABASE kubearchive SET "TimeZone" TO 'UTC';
+
+
+\connect kubearchive
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
 -- Name: trigger_set_timestamp(); Type: FUNCTION; Schema: public; Owner: kubearchive
 --
 

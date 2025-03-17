df3d7aa0eb49c920a6924a63e601aafe0a8abe95
skoved <skoved@redhat.com>
2025-03-17 09:13:10 -0400

    integration test action should fail when go test cmd fails
    I forgot to add `set -o pipefail` in test/integration/run.sh. This cases the integration test action to report as passed
    instead of failed when the `go test` command fails. Also unset errexit before running integration tests so that the test
    logs can be processed and an action summary can be generated even on failure. Return code for the integration test's go
    test command is saved to be used as the return code for the script.
    
    Signed-off-by: skoved <skoved@redhat.com>

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -4,6 +4,7 @@
 
 set -o xtrace
 set -o errexit
+set -o pipefail
 
 RESULTS_DIR="integration-results"
 FILE_PREFIX=$(date +"%d-%b-%Y_%H-%M")
@@ -12,14 +13,18 @@ if [ ! -d "${RESULTS_DIR}" ]; then
     mkdir -p ${RESULTS_DIR}
 fi
 
+# Don't fail immediately so we can generate a summary of the action
+set +o errexit
+
 go test -json -count=1 -v ./test/integration -tags=integration -timeout 60m \
     | tee ${RESULTS_DIR}/${FILE_PREFIX}_results.jsonl \
     | jq -r 'select(.Action == "output") | .Output | rtrimstr("\n")'
 
+# save return code from integration test to use as return code from this script later
+RET_CODE="$?"
+
 jq --slurp -r 'group_by(.Test) | .[] | .[] | select(.Action == "output") | .Output | rtrimstr("\n")' ${RESULTS_DIR}/${FILE_PREFIX}_results.jsonl > ${RESULTS_DIR}/${FILE_PREFIX}_formatted.txt
 
-# Don't fail if we can't generate a summary of the action
-set +o errexit
 
 cat << EOF > ${GITHUB_STEP_SUMMARY:-/dev/stdout}
 ## Integration Test Logs Grouped by Test
@@ -32,3 +37,6 @@ $(cat ${RESULTS_DIR}/${FILE_PREFIX}_formatted.txt)
 $(cat ${RESULTS_DIR}/${FILE_PREFIX}_results.jsonl)
 \`\`\`
 EOF
+
+# use exit code from integration test command as return code for this script
+exit $RET_CODE

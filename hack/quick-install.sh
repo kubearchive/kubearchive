334c85052a31b35106b0c2e6a9f5abf617bea58f
Hector Martinez <hemartin@redhat.com>
2025-03-28 12:01:56 +0100

    Update Eventing to avoid permissions requests
    Signed-off-by: Hector Martinez <hemartin@redhat.com>

--- hack/quick-install.sh
+++ hack/quick-install.sh
@@ -11,11 +11,11 @@ cd ${SCRIPT_DIR}/..
 bash integrations/database/postgresql/install.sh
 
 export CERT_MANAGER_VERSION=v1.9.1
-export KNATIVE_EVENTING_VERSION=v1.15.0
+export KNATIVE_EVENTING_VERSION=v1.17.4
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.yaml
 kubectl apply -f https://github.com/knative/eventing/releases/download/knative-${KNATIVE_EVENTING_VERSION}/eventing.yaml
 kubectl rollout status deployment --namespace=cert-manager --timeout=30s
-kubectl rollout status deployment --namespace=knative-eventing --timeout=50s
+kubectl rollout status deployment --namespace=knative-eventing --timeout=60s
 
 bash hack/kubearchive-install.sh

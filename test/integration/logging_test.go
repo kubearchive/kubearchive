f5131949badd156dc4863226842e2958c55f61cd
Hector Martinez <hemartin@redhat.com>
2025-04-17 11:55:56 +0200

    Fix empty log annotation use-case
    Signed-off-by: Hector Martinez <hemartin@redhat.com>

--- test/integration/logging_test.go
+++ test/integration/logging_test.go
@@ -158,109 +158,78 @@ func TestDefaultContainer(t *testing.T) {
 			},
 		},
 	}
-
 	test.CreateKAC(t, namespaceName, resources)
 
-	// Create a pod
-	pod := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "defaults-to-first",
+	pods := map[string]map[string]string{
+		"defaults-to-first": map[string]string{},
+		"wants-second": map[string]string{
+			"kubectl.kubernetes.io/default-container": "second",
 		},
-		Spec: corev1.PodSpec{
-			RestartPolicy: corev1.RestartPolicyNever,
-			Containers: []corev1.Container{
-				{
-					Name:    "first",
-					Image:   "quay.io/fedora/fedora:latest",
-					Command: []string{"echo", "-n", "I'm the container called first."},
-				},
-				{
-					Name:    "second",
-					Image:   "quay.io/fedora/fedora:latest",
-					Command: []string{"echo", "-n", "I'm the container called second."},
+		"empty-annotation": map[string]string{
+			"kubectl.kubernetes.io/default-container": "",
+		},
+	}
+
+	for podName, annotations := range pods {
+		// Create a pod
+		pod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        podName,
+				Annotations: annotations,
+			},
+			Spec: corev1.PodSpec{
+				RestartPolicy: corev1.RestartPolicyNever,
+				Containers: []corev1.Container{
+					{
+						Name:    "first",
+						Image:   "quay.io/fedora/fedora-minimal:latest",
+						Command: []string{"echo", "-n", "I'm the container called first."},
+					},
+					{
+						Name:    "second",
+						Image:   "quay.io/fedora/fedora-minimal:latest",
+						Command: []string{"echo", "-n", "I'm the container called second."},
+					},
 				},
 			},
-		},
-	}
-	_, err := clientset.CoreV1().Pods(namespaceName).Create(context.Background(), pod, metav1.CreateOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	pod = &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "wants-second",
-			Annotations: map[string]string{
-				"kubectl.kubernetes.io/default-container": "second",
-			}},
-		Spec: corev1.PodSpec{
-			RestartPolicy: corev1.RestartPolicyNever,
-			Containers: []corev1.Container{
-				{
-					Name:    "first",
-					Image:   "quay.io/fedora/fedora:latest",
-					Command: []string{"echo", "-n", "I'm the container called first."},
-				},
-				{
-					Name:    "second",
-					Image:   "quay.io/fedora/fedora:latest",
-					Command: []string{"echo", "-n", "I'm the container called second."},
-				},
-			},
-		},
-	}
-	_, err = clientset.CoreV1().Pods(namespaceName).Create(context.Background(), pod, metav1.CreateOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	url := fmt.Sprintf("https://localhost:%s/api/v1/namespaces/%s/pods/defaults-to-first/log", port, namespaceName)
-	retryErr := retry.Do(func() error {
-		body, err := test.GetLogs(t, token.Status.Token, url)
+		}
+		_, err := clientset.CoreV1().Pods(namespaceName).Create(context.Background(), pod, metav1.CreateOptions{})
 		if err != nil {
-			return err
+			t.Fatal(err)
 		}
-
-		if len(body) == 0 {
-			return errors.New("could not retrieve the pod log")
-		}
-		t.Log("Successfully retrieved logs")
-
-		bodyString := string(body)
-		if strings.Trim(bodyString, "\n") != "I'm the container called first." {
-			t.Log("log does not match")
-			return fmt.Errorf("log does not match the expected 'I'm the container called first.'")
-		}
-
-		return nil
-	}, retry.Attempts(30), retry.MaxDelay(2*time.Second))
-
-	if retryErr != nil {
-		t.Fatal(retryErr)
 	}
 
-	url = fmt.Sprintf("https://localhost:%s/api/v1/namespaces/%s/pods/wants-second/log", port, namespaceName)
-	retryErr = retry.Do(func() error {
-		body, err := test.GetLogs(t, token.Status.Token, url)
-		if err != nil {
-			return err
+	expected := map[string]string{
+		"defaults-to-first": "I'm the container called first.",
+		"wants-second":      "I'm the container called second.",
+		"empty-annotation":  "I'm the container called first.",
+	}
+
+	for podName, expectedLog := range expected {
+		t.Logf("checking logs for pod '%s', expected log '%s'", podName, expectedLog)
+		url := fmt.Sprintf("https://localhost:%s/api/v1/namespaces/%s/pods/%s/log", port, namespaceName, podName)
+		retryErr := retry.Do(func() error {
+			body, err := test.GetLogs(t, token.Status.Token, url)
+			if err != nil {
+				return err
+			}
+
+			if len(body) == 0 {
+				return errors.New("could not retrieve the pod log")
+			}
+			t.Log("Successfully retrieved logs")
+
+			bodyString := string(body)
+			if strings.Trim(bodyString, "\n") != expectedLog {
+				t.Log("log does not match")
+				return fmt.Errorf("log does not match the expected '%s'", expectedLog)
+			}
+
+			return nil
+		}, retry.Attempts(30), retry.MaxDelay(5*time.Second))
+
+		if retryErr != nil {
+			t.Fatal(retryErr)
 		}
-
-		if len(body) == 0 {
-			return errors.New("could not retrieve the pod log")
-		}
-		t.Log("Successfully retrieved logs")
-
-		bodyString := string(body)
-		if strings.Trim(bodyString, "\n") != "I'm the container called second." {
-			t.Log("log does not match")
-			return fmt.Errorf("log does not match the expected 'I'm the container called second.'")
-		}
-
-		return nil
-	}, retry.Attempts(30), retry.MaxDelay(2*time.Second))
-
-	if retryErr != nil {
-		t.Fatal(retryErr)
 	}
 }

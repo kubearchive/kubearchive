6736021a8b3180d0a92749153f7af227c1edc8f1
obetsun <obetsun@redhat.com>
2025-06-25 18:40:03 +0300

    correct the uninstall script
    Signed-off-by: obetsun <obetsun@redhat.com>

--- integrations/logging/loki/uninstall.sh
+++ integrations/logging/loki/uninstall.sh
@@ -2,7 +2,9 @@
 # Copyright KubeArchive Authors
 # SPDX-License-Identifier: Apache-2.0
 NAMESPACE="grafana-loki"
+VECTOR_NAMESPACE="kubearchive-vector"
 
 kubectl delete namespace ${NAMESPACE}
+kubectl delete namespace ${VECTOR_NAMESPACE}
 kubectl delete clusterrole logging-operator logging-operator-edit
 kubectl delete clusterrolebinding logging-operator

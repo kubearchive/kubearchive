83d96e50541a3f22c7c30bbb13730a4c793cc162
Sam Koved <skoved@redhat.com>
2025-11-05 11:14:10 -0500

    add index for uuid in the log_url table (#1461)
    uuid is the value that the sink uses for looking up log_urls to delete.
    adding this index should speed up these delete operations.
    
    Signed-off-by: skoved <skoved@redhat.com>

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -13,7 +13,7 @@ import (
 	"github.com/kubearchive/kubearchive/pkg/database/sql"
 )
 
-var CurrentDatabaseSchemaVersion = "2"
+var CurrentDatabaseSchemaVersion = "3"
 var RegisteredDatabases = map[string]interfaces.Database{
 	"postgresql": sql.NewPostgreSQLDatabase(),
 	"mariadb":    sql.NewMariaDBDatabase(),

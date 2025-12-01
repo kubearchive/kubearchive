944594e13d85b5ad8ce6572f13b1bf4b7286f2eb
Hector Martinez <hemartin@redhat.com>
2025-12-01 16:01:19 +0100

    Create new package for tools
    Signed-off-by: Hector Martinez <hemartin@redhat.com>

---
+++ cmd/tools/main.go
@@ -0,0 +1,10 @@
+// Copyright KubeArchive Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is a fake entrypoint to create a container with tools.")
+}
